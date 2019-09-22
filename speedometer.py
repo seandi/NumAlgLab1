4f44ce37db31da82bfd66ce1033b386956cc7ddd
seandi <eandi.simo@outlook.it>
2019-09-22 22:04:48 +0200

    Add positive speed constraint and convergence threshold

--- speedometer.py
+++ speedometer.py
@@ -9,6 +9,9 @@ class SpeedometerSolver:
         self.speeds = list(map(int, file.readline().strip('\n').split()))
         file.close()
 
+        self.c = []
+        self.threshold = 1e-7
+
     def solve_equation(self, c):
         s = 0
         for di, si in zip(self.distances, self.speeds):
@@ -22,22 +25,40 @@ class SpeedometerSolver:
         return s
 
     def test_solution(self, c):
-        time = 0;
+        time = 0
         for di, si in zip(self.distances, self.speeds):
             time += di / (si + c)
         return self.T - time
 
-    def newtons_method(self, c0):
-        self.c = c0
-        for i in range(0, 10):
-            self.c -= (self.solve_equation(self.c) / self.solve_derivative(self.c))
-            print(self.c)
+    def delta(self, iteration):
+        return abs(self.c[iteration]-self.c[iteration-1])
 
-    def run(self, c0):
-        self.newtons_method(c0)
-        print("Time error: {0}".format(self.test_solution(self.c)))
+    def newtons_method(self, c0):
+        self.c = []
+        self.c.insert(0, c0)
+        i = 0;
+
+        while True:
+            i += 1;
+            self.c.insert(i, self.c[i-1] - (self.solve_equation(self.c[i-1]) / self.solve_derivative(self.c[i-1])))
+            print("Iteration {0} -> {1}".format(i, self.c[i]))
+            if self.delta(i) < self.threshold:
+                print("Solution found: {}".format(self.c[i]))
+                if self.c[i] >= -min(self.speeds):
+                    print(" satisfies positive speed constraint!")
+                else:
+                    print(" does NOT satisfies positive speed constraint!")
+                break
+            elif (i > 1) and (self.delta(i) >= self.delta(i-1)):
+                print("The solution does not converge: try with a different initial value")
+                break
+
+    def run(self, initial_guess, threshold=1e-7):
+        self.threshold = threshold
+        self.newtons_method(initial_guess)
+        print("Time error: {0}".format(self.test_solution(self.c.pop())))
 
 
 if __name__ == "__main__":
     solver = SpeedometerSolver('input.txt')
-    solver.run(-0.1)
+    solver.run(-0.4, threshold=1e-29)
